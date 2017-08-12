d98a20a05d0e1a166005721aac57603b14ba6b5c
Rob Allsopp <rca06d@my.fsu.edu>
2017-08-12 09:14:54 -0600

    extend header for more flexible storage of labels and features

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # training-data-generator
 This takes an audio file and a midi file, chops up the audio at each midi note, and spits out a file of raw training examples and a matching file of labels according to the midi mapping
+
+# Output File Spec
+Everything is little-endian encoded, and packed without junk or padding. 
+### Header
+- **Header Id**, `4 bytes, string === 'NDAT'`
+- **Number of features**, `4 bytes, uint`, how many input features there are in each training example
+- **Feature Format**, `2 bytes, uint === [1, 2, 3, 4]`, format for each feature. `1` for float32, `2` for int32, `3` for int16, and `4` for int8
+- **Number of Labels**, `4 bytes, uint`, how many output labels there are in each training example
+- **Label Format**, `2 bytes, uint === [1, 2, 3, 4]`, format for each label. `1` for float32, `2` for int32, `3` for int16, and `4` for int8
+- **Number of Examples**, `4 bytes, uint`, how many training examples there are
+- **Label Offset**, `4 bytes, int`, how far the labels are offset from the location of the audio feature
+### Body
+Each training example consists of a feature set and a label set. Each label set immediately follows the feature set to which it belongs.
+
+Given the following header:
+```javascript
+const header = {
+	id: 'NDAT',
+	numFeatures: 4,
+	featureFormat: 1,
+	numLabels: 3,
+	labelFormat: 4,
+	numExamples: 100
+}
+```
+The body should be packed as follows:
+![alt text][file_format]
+
+[file_format]: /docs/file_format.png "NDAT body format"
--- training-ex-writer/TrainingDataHeader.js
+++ training-ex-writer/TrainingDataHeader.js
@@ -1,39 +1,56 @@
 const fs = require('fs');
 
 class TrainingDataHeader {
-	constructor({numFeatures, numLabels, numExamples, labelOffset = 0}) {
+	constructor({
+								numFeatures,
+								numLabels,
+								numExamples,
+								featureFormat = TrainingDataHeader.FMT_FLOAT,
+								labelFormat = TrainingDataHeader.FMT_FLOAT,
+								labelOffset = 0
+	}) {
 		this.numFeatures = numFeatures;
+		this.featureFormat = featureFormat;
 		this.numLabels = numLabels;
+		this.labelFormat = labelFormat;
 		this.numExamples = numExamples;
 		this.labelOffset = labelOffset;
 	}
 
 	toBuffer() {
-		const buffer = Buffer.alloc(20);
-		buffer.write('NDAT', 0, 4);
+		const buffer = Buffer.alloc(24);
+		buffer.write(TrainingDataHeader.HEADER_ID, 0, 4);
 		buffer.writeUInt32LE(this.numFeatures, 4);
-		buffer.writeUInt32LE(this.numLabels, 8);
-		buffer.writeUInt32LE(this.numExamples, 12);
-		buffer.writeInt32LE(this.labelOffset, 16);
+		buffer.writeUInt16LE(this.featureFormat, 8);
+		buffer.writeUInt32LE(this.numLabels, 10);
+		buffer.writeUInt16LE(this.labelFormat, 14);
+		buffer.writeUInt32LE(this.numExamples, 16);
+		buffer.writeInt32LE(this.labelOffset, 20);
 		return buffer;
 	}
 
 	static get HEADER_ID () { return 'NDAT'; }
-	static get HEADER_SIZE () { return 20; }
+	static get HEADER_SIZE () { return 24; }
+	static get FMT_FLOAT () { return 1; }
+	static get FMT_INT32 () { return 2; }
+	static get FMT_INT16 () { return 3; }
+	static get FMT_INT8 () { return 4; }
 
 	static fromFile(fd) {
-		const buffer = Buffer.alloc(20);
-		fs.readSync(fd, buffer, 0, 20, 0);
+		const buffer = Buffer.alloc(24);
+		fs.readSync(fd, buffer, 0, 24, 0);
 
 		const id = buffer.toString('utf8', 0, 4);
 
-		if (id !== 'NDAT') throw new Error(`Unrecognized header id: ${id}`);
+		if (id !== TrainingDataHeader.HEADER_ID) throw new Error(`Unrecognized header id: ${id}`);
 
 		return new TrainingDataHeader({
 			numFeatures: buffer.readUInt32LE(4),
-			numLabels: buffer.readUInt32LE(8),
-			numExamples: buffer.readUInt32LE(12),
-			labelOffset: buffer.readInt32LE(16)
+			featureFormat: buffer.readUInt16LE(8),
+			numLabels: buffer.readUInt32LE(10),
+			labelFormat: buffer.readUInt16LE(14),
+			numExamples: buffer.readUInt32LE(16),
+			labelOffset: buffer.readInt32LE(20)
 		});
 	}
 }
