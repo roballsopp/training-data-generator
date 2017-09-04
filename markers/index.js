3183343657d0fdd9ecd2796fea55dad747c3f4bb
Rob Allsopp <rca06d@my.fsu.edu>
2017-09-04 17:30:03 -0600

    switch to better midi lib

--- markers/index.js
+++ markers/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const Promise = require('bluebird');
 const _ = require('lodash');
-const { parseMidi } = require('midi-file');
+const MIDIFile = require('midifile');
+const MIDIEvents = require('midievents');
 const { ARTICULATIONS, NUM_ARTICULATIONS } = require('./midi-map.config');
 const fsReadFile = Promise.promisify(fs.readFile);
 
@@ -20,8 +21,8 @@ class Markers {
 		const midiMap = require(mapPath)(ARTICULATIONS);
 
 		return fsReadFile(markerPath)
-			.then(parseMidi)
-			.then(midiFile => new Markers(midiFile, midiMap))
+			.then(f => new MIDIFile(f.buffer))
+			.then(m => new Markers(m.header.getTicksPerBeat(), m.getTrackEvents(0), midiMap))
 	}
 
 	static generateNegativeMarkers(positiveMarkers, minDistanceFromPositiveMarkers = 0) {
@@ -48,9 +49,9 @@ class Markers {
 		return pos && { pos: Math.round(pos), y: new Int8Array(NUM_ARTICULATIONS) };
 	}
 
-	constructor(midiFile, midiMap) {
-		this._ticksPerBeat = midiFile.header.ticksPerBeat;
-		this._events = midiFile.tracks[0];
+	constructor(ticksPerBeat, events, midiMap) {
+		this._ticksPerBeat = ticksPerBeat;
+		this._events = events;
 
 		if (!Array.isArray(midiMap)) throw new Error(`Invalid midi map. Expected array, got ${typeof midiMap}`);
 		if (midiMap.length !== 128) throw new Error(`Invalid midi map. Expected length 128. Got length ${midiMap.length}`);
@@ -72,15 +73,16 @@ class Markers {
 
 		for (let i = 0, l = this._events.length; i < l; i++) {
 			const evt = this._events[i];
-			const numBeatsSinceLastEvt = evt.deltaTime / this._ticksPerBeat;
+			const numBeatsSinceLastEvt = evt.delta / this._ticksPerBeat;
 
 			elapsedSamples += (numBeatsSinceLastEvt * currentSampPerBeat);
 
-			if (evt.type === 'setTempo') currentSampPerBeat = (evt.microsecondsPerBeat / MuS_PER_SECOND) * sampleRate;
-			else if (evt.type === 'noteOn') {
+			if (evt.subtype === MIDIEvents.EVENT_META_SET_TEMPO) currentSampPerBeat = (evt.tempo / MuS_PER_SECOND) * sampleRate;
+			else if (evt.subtype === MIDIEvents.EVENT_MIDI_NOTE_ON) {
 				const markerPosition = Math.round(elapsedSamples);
 				markerMap[markerPosition] = markerMap[markerPosition] || new Int8Array(NUM_ARTICULATIONS);
-				const mappedNote = this._midiMap[evt.noteNumber]; // map 128 possible midi notes into NUM_ARTICULATIONS
+				// param1 is note number, param2 is velocity
+				const mappedNote = this._midiMap[evt.param1]; // map 128 possible midi notes into NUM_ARTICULATIONS
 				if (mappedNote !== ARTICULATIONS.NO_HIT) markerMap[markerPosition][mappedNote] = 1;
 			}
 		}
